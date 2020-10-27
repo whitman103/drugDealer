ff37c12e22f8372256c99157d00d7b768490cc5d
whitman103 <64374101+whitman103@users.noreply.github.com>
2020-10-27 18:25:09 -0400

    imageUpdate

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # A work in progress C++ agent based model, attempting to model the three-pronged interaction between drug dealers, a user population, and an enforcing population.
 The main idea of this model is to intake a map of a real section of a city, currently a set of blocks in downtown Columbus, Ohio, and simulate the interactions amongst a group of drug dealers, drug users/seekers, and the enforcing body. The goal of the model is to understand how physical setup of a city can influence the successful control of a drug dealing enterprise, investigate the spatial effects of the environment, such as public transportation, sprawl, and roadways on drug policy, and other goals which benefit from an explicitly spatial and individual based approach to modeling.
 ## Model is written in C++, image analysis performed in Python, map generated in qGIS, data from the City of Columbus.
+Example of Street corner identification performed in program using OpenCV2 in Python.
+[Street Image](https://github.com/whitman103/drugDealer/blob/master/nodeId.PNG)
--- mapStuff.py
+++ mapStuff.py
@@ -49,6 +49,7 @@ foundImg=findCrossing(radius,img2)
 cv2.imwrite("crossingImage.png",foundImg)"""
 
 foundImg=cv2.imread("crossingImage.png")
+cv2.imshow("FoungImg",foundImg)
 
 def returnAdjacency(x,y):
 	return [[x,y-1],[x,y+1],[x+1,y],[x-1,y]]
@@ -72,7 +73,7 @@ def consolidateCrossings(img):
 					crossingDictionary.append([])
 	return crossingDictionary
 
-crossingIdentities=consolidateCrossings(foundImg)
+"""crossingIdentities=consolidateCrossings(foundImg)
 
 
 def findCentroids(inArray):
@@ -87,51 +88,98 @@ def findCentroids(inArray):
 	
 centroids=findCentroids(crossingIdentities)
 
+
+
+outFile=open("baseCentroids.txt",'w')
+centroids.pop(0)
+for element in centroids:
+	outFile.write(str(element[0])+" "+str(element[1])+"\n")
+outFile.close()"""
+
+def loadCentroids(inString):
+
+	centroids=[]
+	inString=open(inString,'r')
+	for line in inString:
+		line=line.split(" ")
+		line[1]=line[1].split("\n")[0]
+		centroids.append([int(line[0]),int(line[1])])
+	return centroids
+
+
+centroids=loadCentroids("baseCentroids.txt")
+
 testCentroids=foundImg
+
 for yIndex,yData in enumerate(foundImg):
 	for xIndex,xData in enumerate(foundImg[yIndex]):
 		if [yIndex,xIndex] in centroids:
-			testCentroids[yIndex][xIndex]=[255,0,0]
+			testCentroids[yIndex][xIndex]=[0,255,0]
 		else:
 			testCentroids[yIndex][xIndex]=[0,0,0]
-cv2.imshow("testImg",testCentroids)
-cv2.waitKey(0)
 
-def distance(x1,x2,y1,y2):
+cv2.imshow("greenTest",testCentroids)
+
+
+
+def distance(x1,y1,x2,y2):
 	return math.sqrt(pow(x1-x2,2)+pow(y1-y2,2))
 
 def averageCentroids(minDistance,inCentroids):
 	mergeList=[[] for i in range(len(inCentroids))]
 	for firstIndex,firstData in enumerate(inCentroids):
+		selfInclude=True
 		for secondIndex,secondData in enumerate(inCentroids):
 			if distance(inCentroids[firstIndex][0],inCentroids[firstIndex][1],inCentroids[secondIndex][0],inCentroids[secondIndex][1])<minDistance:
 				mergeList[firstIndex].append(secondIndex)
-	trashList=[]
-	print(mergeList)
-	averagedCentroids=[[]]
-	for mergeGroup in mergeList:
-		check=True
-		for index in mergeGroup:
-			if index in trashList:
-				check=False
+		
+			
+	finalList=[]
+	checkBack=[i for i in range(len(inCentroids))]
+	for mergeIndex,group in enumerate(mergeList):
+		if mergeIndex==0:
+			finalList.append(group)
+		for element in group:
+			appendGroup=True
+			for finalIndex,finalGroup in enumerate(finalList):
+				if element in finalGroup:
+					finalList[finalIndex]+=list(set(group)-set(finalGroup))
+					appendGroup=False
+			if appendGroup:
+				finalList.append(group)
+	print(finalList)
+	outList=[]
+	for mergeGroup in finalList:
 		xMean,yMean=0,0
-		if check:
-			for index in mergeGroup:
-				xMean+=inCentroids[index][1]/len(mergeGroup)
-				yMean+=inCentroids[index][0]/len(mergeGroup)
-			averagedCentroids.append([math.floor(yMean),math.floor(xMean)])
-			trashList.append(mergeGroup)
-	return averagedCentroids
-centroids.pop(0)
+		for index in mergeGroup:
+			xMean+=inCentroids[index][1]/len(mergeGroup)
+			yMean+=inCentroids[index][0]/len(mergeGroup)
+		outList.append([int(yMean),int(xMean)])
+	outList.pop(0)
+	return outList
+
+
 centroids=averageCentroids(15,centroids)
-print(centroids)
-for yIndex,yData in enumerate(foundImg):
-	for xIndex,xData in enumerate(foundImg[yIndex]):
+
+
+
+
+
+
+mergedCentroids=foundImg
+for yIndex,yData in enumerate(mergedCentroids):
+	for xIndex,xData in enumerate(mergedCentroids[yIndex]):
 		if [yIndex,xIndex] in centroids:
-			testCentroids[yIndex][xIndex]=[255,0,0]
+			for xSweep in range(-1,2,1):
+				for ySweep in range(-1,2,1):
+					mergedCentroids[yIndex+ySweep][xIndex+xSweep]=[255,0,0]
 		else:
-			testCentroids[yIndex][xIndex]=[0,0,0]
-cv2.imshow("testImg",testCentroids)
+			if mergedCentroids[yIndex][xIndex][0]!=255:
+				mergedCentroids[yIndex][xIndex]=[0,0,0]
+
+
+
+cv2.imshow("Compare",mergedCentroids)
 cv2.waitKey(0)
 
 """for yIndex,data in enumerate(img2):
